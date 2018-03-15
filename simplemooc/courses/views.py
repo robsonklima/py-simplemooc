cd609bacaaf068e0e034c6978613ac13731f75a2
robsonklima <robsonklima@gmail.com>
2018-03-15 13:16:48 -0300

    REF

--- simplemooc/courses/views.py
+++ simplemooc/courses/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
+from .models import Course
+
 def courses(request):
+    courses = Course.objects.all()
+    
+    context = {
+        'courses': courses
+    }
+
     template_name = "courses.html"
-    return render(request, template_name)
+    return render(request, template_name, context)
