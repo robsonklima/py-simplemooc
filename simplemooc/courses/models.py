6ad14106e800cd9660ea56fbe0078dcbcaad4550
robsonklima <robsonklima@gmail.com>
2018-03-29 21:34:07 -0300

    REF

--- simplemooc/courses/models.py
+++ simplemooc/courses/models.py
@@ -11,6 +11,7 @@ class Course(models.Model):
     name = models.CharField('Nome', max_length=100)
     slug = models.SlugField('Atalho')
     description = models.TextField('Descrição', blank=True)
+    about = models.TextField('Sobre', blank=True)
     start_date = models.DateField('Data de Início', null=True, blank=True)
     image = models.ImageField(upload_to='courses/images', verbose_name='Imagens',null=True,blank=True)
     created_at = models.DateTimeField('Criado em', auto_now_add=True)
